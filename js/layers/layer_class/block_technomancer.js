8d85b56a10125e7027554fa8aa24c863c76dd4b3
The Chiprel <wretchedrus@ya.ru>
2023-03-28 22:09:42 +0300

    Добавил разметку техноманта

--- js/layers/layer_class/block_technomancer.js
+++ js/layers/layer_class/block_technomancer.js
@@ -207,6 +207,170 @@ function Block_Technomancer_Hacks_t(class_data){
 //additional initialization
     Init();
 }
+/*
+//public methods
+function Block_Technomancer_Spells_t(class_data){
+//constants
+const SPELLS_KNOWN = [
+    [ 1,  1,  1,  1,  2,  3], //0 lvl
+    [ 1,  1,  2,  3,  7, 11], //1 lvl
+    [ 4,  4,  5,  6, 10, 14], //2 lvl
+    [ 7,  7,  8,  9, 13, 17], //3 lvl
+    [10, 10, 11, 12, 16, 20], //4 lvl
+    [13, 13, 14, 15, 19    ], //5 lvl
+    [16, 16, 17, 18, 20    ]  //6 lvl
+];
+
+const SPELL_LIST_TABLE_ID = "table_spell_list_technomancer";
+
+//private methods
+    var Init = function(){
+        while (m_table.rows.length > 1){
+            m_table.deleteRow(1);
+        }
+
+        let lvl_list = m_class_data.hacks.Get_Lvl_List();
+        for (let i = 0; i < lvl_list.length; i++){
+            let cur_lvl = lvl_list[i];
+
+            var row = m_table.insertRow(m_table.rows.length);
+            var cell_lvl = row.insertCell(0);
+            var cell_ability = row.insertCell(1);
+            var cell_add_remove_button = row.insertCell(2);
+
+            cell_lvl.innerHTML = cur_lvl;
+            cell_ability.innerHTML = "---";
+
+            var add_func = self.Open_Database.bind(null, i);
+            var add_remove_button = HTML_Create_Button("+", add_func); //TODO
+            cell_add_remove_button.appendChild(add_remove_button);
+        }
+    }
+
+//public methods
+    this.Open_Database = function(row){
+        let lvl_list = m_class_data.hacks.Get_Lvl_List();
+        let max_lvl = lvl_list[row];
+        m_database = Ability_Database_GetList(ABILITIES_DATABASE, "Класс", ["Техномант", "Магический хак"], undefined, max_lvl, true);
+
+        var table_data = new Array(0);
+        var filters = new Array(0);
+        var add_func = self.Set_By_Entry_Num.bind(null, row);
+
+        let headers = [
+            "Название",
+            "Ур.",
+            "Описание",
+            "Ист."
+        ];
+
+        for (let i = 0; i < m_database.length; i++){
+            let cur_ability = m_database[i];
+
+            table_data.push([
+                cur_ability.name,
+                cur_ability.lvl,
+                cur_ability.descr,
+                cur_ability.source]);
+        }
+
+        filters.push(new Database_Filter_Input_t(POPUP_FILTER_TYPES.FIND,    0, "Название"  ));
+        filters.push(new Database_Filter_Input_t(POPUP_FILTER_TYPES.RANGE,   1, "Ур."  ));
+        filters.push(new Database_Filter_Input_t(POPUP_FILTER_TYPES.SELECT,  3, "Источник"  ));
+
+        Popup_Database.Open(
+            table_data,
+            add_func,
+            filters,
+            headers,
+            self.Show_Info_Database);
+    }
+    
+    this.Set = function(row, entry){
+        let table_row = m_table.rows[row + 1]; // +1 with header
+        let cell_name = table_row.cells[1];
+        let cell_add_remove_button = table_row.cells[2];
+        var remove_func = self.Remove.bind(null, row);
+        var show_details_func = self.Show_Details.bind(null, row);
+
+        cell_name.innerHTML = entry.name;
+        cell_name.onclick = show_details_func;
+
+        cell_add_remove_button.innerHTML = "";
+        var add_remove_button = HTML_Create_Button("X", remove_func);
+        cell_add_remove_button.appendChild(add_remove_button);
+
+        m_class_data.hacks.Set(row, entry);
+
+        m_database = null;
+        Popup_Database.Close();
+    }
+
+    this.Set_By_Entry_Num = function(row, entry_num){
+        self.Set(row, m_database[entry_num]);
+    }
+    
+    this.Set_By_Name = function(row, name){
+        var entry = Get_Ability_Entry_By_Name(ABILITIES_DATABASE, name);
+        if (entry == null){
+            console.error("Failed to find ability: '" + name + "'");
+            return;
+        }
+        
+        self.Set(row, entry);
+    }
+
+    this.Remove = function(row){
+        let table_row = m_table.rows[row + 1]; // +1 with header
+        let cell_name = table_row.cells[1];
+        let cell_add_remove_button = table_row.cells[2];
+        var add_func = self.Open_Database.bind(null, row);
+
+        m_class_data.hacks.Remove(row);
+
+        cell_name.innerHTML = "---";
+        cell_name.removeAttribute("onclick");
+
+        cell_add_remove_button.innerHTML = "";
+
+        var add_remove_button = HTML_Create_Button("+", add_func);
+        cell_add_remove_button.appendChild(add_remove_button);
+    }
+
+    this.Show_Details = function(row){
+        m_class_data.hacks.Show_Details(row);
+    }
+
+    this.Show_Info_Database = function(entry_num){
+        let ability = m_database[entry_num];
+
+        Popup_Descr.Call(ability.name, ability.descr);
+    }
+
+    this.Load_From_Obj = function(obj){
+        if (obj == undefined){
+            return;
+        }
+
+        for (let i = 0; i < obj.length; i++){
+            if (obj[i] != null){
+                self.Set_By_Name(i, obj[i]);
+            }
+        }
+    }
+
+//private properties
+    var self = this;
+    var m_class_data = class_data;
+    var m_tables; //TODO
+    var m_database = null;
+
+//public properties
+
+//additional initialization
+    Init();
+}
+*/
 
 function Block_Class_Technomancer_t(){
 
