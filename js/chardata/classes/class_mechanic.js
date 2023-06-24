dea37dbc04f5ca40fe7db94d22febe1ebcc6e385
unknown <bped4ed@mail.ru>
2023-06-24 15:31:17 +0300

    Реализации Механика: добавил способности класса и трюки

--- js/chardata/classes/class_mechanic.js
+++ js/chardata/classes/class_mechanic.js
@@ -1,28 +1,82 @@
 function Class_Mechanic_t (){
-//constants
-// const CLASS_ABILITY_COUNT = 6;
+const ABILITY_LIST = [
+    ["Искусственный интеллект"],
+    ["Обход"],
+    ["Специнструмент"],
+    ["Трюк механика"],
+    ["Перегрузка"],
+    ["Привычное оружие [Механик]"],
+    ["Удалённый взлом"],
+    ["Золотые руки"],
+    ["Профессиональный инструмент"],
+    ["Перехват управления"],
+    ["Слаженная атака"],
+    ["Улучшенный инструмент"],
+    ["Распределённое управление"],
+    ["Превосходный инструмент"],
+    ["Призрак в машине"],
+    ["Мастер технологий"]
+];
+const CLASS_ABILITY_LVLS = [1, 1, 1, 2, 3, 3, 5, 7, 7, 9, 11, 13, 17, 19, 19, 20];
+const TRICKS_LVLS = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
 //private methods
-
+    var Init = function(){
+        let db = Ability_Database_GetList(ABILITIES_DATABASE, "Класс", ["Механик", "Способность класса"]);
+        for (let i = 0; i < ABILITY_LIST.length; i++){
+            if (ABILITY_LIST[i].length == 1){
+                let abi_name_full = ABILITY_LIST[i][0];
+                let [abi_name, abi_suffix] = Split_Ability_Name_Suffix(abi_name_full);
+                let abi_entry = Get_Ability_Entry_By_Name(db, abi_name);
+                self.class_abilities.Set(i, abi_entry, abi_suffix);
+            }else{
+                //TODO?
+            }
+        }
+    }
 
 //public methods
     this.Set_Lvl = function(lvl){
         self.lvl = lvl;
+        self.class_abilities.Update_Lvl(self.lvl);
+        self.tricks.Update_Lvl(self.lvl);
     }
 
     this.Get_SaveData_Obj = function(){
         var ret = {
-            lvl: self.lvl
+            lvl: self.lvl,
+            tricks: self.tricks.Get_SaveData_Obj()
         }
         return ret;
+    }
+    
+    this.Load_From_Obj = function(obj){
+        if (obj == undefined){
+            return;
+        }
+        
+        self.tricks.Load_From_Obj(obj.tricks);
     }
 
 //private properties
     var self = this;
-    //var m_class_abilities = new Ability_Collection_t("Классовые способности (Механик)", CLASS_ABILITY_COUNT);
 
 //public properties
     this.lvl = 0;
+    this.class_abilities = new Leveled_Ability_List_t(
+        "abi_class_mechanic",
+        "Классовые способности (Механик)",
+        CLASS_ABILITY_LVLS,
+        "mechanic_class_",
+        layers.classes.Get_Block(CLASSES.MECHANIC).class_abilities,
+        true);
+    this.tricks = new Leveled_Ability_List_t(
+        "abi_class_mechanic_tricks",
+        "Трюки механика",
+        TRICKS_LVLS,
+        "mechanic_tricks_",
+        layers.classes.Get_Block(CLASSES.MECHANIC).tricks);
 
 //additional initialization
+    Init();
 }
--- js/layers/layer_class/block_mechanic.js
+++ js/layers/layer_class/block_mechanic.js
@@ -1,41 +1,6 @@
 function Block_Class_Mechanic_t(){
-//constants
-// const ENVOY_ABILITY_LIST = [
-    // ["Импровизация посланника"],
-    // ["Мастерство"],
-    // ["Мастерство в навыке"],
-    // ["Мастерский талант"],
-    // ["Привычное оружие [Посланник]"],
-    // ["Истинный мастер"]
-// ];
-    
 //private methods
     var Init = function(){
-        while (m_class_abi_table.rows.length > 1){
-            m_class_abi_table.deleteRow(1);
-        }
-        
-        ENVOY_ABILITY_LIST.forEach(ability => {
-            if(ability.length == 1){
-                var entry = Find_Ability_By_Name(ability[0]);
-                if (entry != null){
-                    var row = m_class_abi_table.insertRow(m_class_abi_table.rows.length);
-            
-                    var cell_lvl = row.insertCell(0);
-                    var cell_ability = row.insertCell(1);
-                    
-                    if (entry.lvl != null){
-                        cell_lvl.innerHTML = entry.lvl;
-                    }
-            
-                    cell_ability.innerHTML = entry.name;
-                }else{
-                    console.error("Attempting to add unknown ability to ability table: '" + ability[0] + "'");
-                }
-            }
-            //else: TODO
-        });
-        
         self.Hide_Block();
     }
 
@@ -43,7 +8,7 @@ function Block_Class_Mechanic_t(){
     this.Show_Block = function(){
         m_block.style.display = "block";
     }
-    
+
     this.Hide_Block = function(){
         m_block.style.display = "none";
     }
@@ -51,10 +16,19 @@ function Block_Class_Mechanic_t(){
 //private properties
     var self = this;
     var m_block = document.getElementById("block_class_details_mechanic");
-    //var m_class_abi_table = document.getElementById('table_class_abilities_envoy');
 
 //public properties
+//TODO: get levels properly
+    this.class_abilities = new Block_Ability_List_t(
+        document.getElementById('table_class_abilities_mechanic'),
+        null
+    );
+    
+    this.tricks = new Block_Ability_List_t(
+        document.getElementById('table_class_mechanic_tricks'),
+        Ability_Database_GetList(ABILITIES_DATABASE, "Класс", ["Механик", "Трюк механика"], undefined, 20, true)
+    );
 
 //additional initialization
-    //Init();
+    Init();
 }
